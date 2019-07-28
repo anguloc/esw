4df5742a5b718bb9aee8d377256ba9fdecb852bd
gk <xy62201793@163.com>
2019-07-28 18:28:02 +0800

    push

--- Esw/Controller/Http/Index.php
+++ Esw/Controller/Http/Index.php
@@ -11,7 +11,7 @@ class Index extends Controller
     public function index()
     {
 //        $this->actionNotFound('index');
-        
+
         $this->response()->withStatus(Status::CODE_NOT_FOUND);
         $this->response()->write('not exist http site');
         $this->response()->end();
